c4ef948e67f58fb8483dc03d756ad6073d6584b1
Dung Tran <dutran@zendesk.com>
2019-08-06 20:10:31 +0800

    Update client peer logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,92 @@
 import React, { Component } from "react";
-import io from 'socket.io-client';
-import { Store } from
-import { Messages } from './utils/constants';
+import io from "socket.io-client";
+import { ServerMessages, PeerMessages } from "./utils/constants";
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    const socket = io.connect('http://localhost:4321');
+    const socket = io.connect("http://localhost:4321");
 
-    socket.on('connect', this.handleConnect);
-    socket.on('event', this.handleEvent);
-    socket.on('disconnect', this.handleDisconnect);
+    this.id = null;
+    this.peerConnections = {};
+    this.peerSignaler = null;
+    this.peerSignaler.on('connection', this.handlePeerConnection);
+
+    this.state = {
+      snapshot: {}
+    };
+
+    socket.on("connect", this.handleServerConnect);
+    socket.on("event", this.handleServerEvent);
+    socket.on("disconnect", this.handleServerDisconnect);
   }
 
-  handleConnect(socket) {
-    this.setState({ status: 'CONNECTED' });
+  handleServerConnect(socket) {
+    this.setState({ status: "CONNECTED" });
   }
 
-  handleEvent(event) {
+  handleServerEvent(event) {
     switch (event.type) {
-      case 'set_id':
-        // set peer id
-        // send data request
-        break;
-      case 'data':
-        store.update(event.data);
+      case ServerMessages.Incoming.SetId:
+        this.id = event.id;
+        this.peerSignaler = new Peer(id, {
+          host: "localhost",
+          port: 1234,
+          path: "/peer"
+        });
+        this.peerSignaler.on("conne")
         break;
 
-      case 'add_edge':
-        if (event.isInitiator) {
-          // form peer link
+      case ServerMesages.Incoming.Data:
+        if (event.data.snapshot) {
+          // save snapshot, this is first event
+        } else if (event.data.messages) {
+          // gossip to peers
         }
         break;
-      case 'remove_edge':
+
+      case ServerMessages.Incoming.AddEdge:
         if (event.isInitiator) {
-          // remove peer link
+          this.peerConnections[event.neighborId] = this.peerSignaler.connect(event.neighborId);
+          this.peerConnections[event.neighborId].on('open', this.handlePeerOpen);
+          this.peerConnections[event.neighborId].on('data', this.handlePeerData);
         }
         break;
+
+      case ServerMessages.Incoming.RemoveEdge:
+        if (event.isInitiator) {
+          this.peerConnections[event.neighborId] = this.peerSignaler.disconnect(event.neighborId);
+          this.peerConnections[event.neighborId].on('close', this.handlePeerClose);
+        }
+        break;
+
       default:
-        console.log('UNEXPECTED EVENT:', event);
+        console.log("UNEXPECTED EVENT:", event);
     }
   }
 
+  handleServerDisconnect() {
+    this.setState({ serverStatus: "DISCONNECTED" });
+  }
+
+  handlePeerOpen() {}
+  handlePeerClose() {}
+  handlePeerConnection(conn) {
+    conn.on('data', data => {
+      this.state.set({
+        messages: [this.state.messages..., data.messages...]
+      });
+    });
+  }
+  handlePeerDisconnection() {}
+
   handlePeerEvent() {
-    if follower
-      switch (type) {
-        case data
-
-      }
-    if leader
 
   }
 
-  handleDisconnect() {
-    this.setState({ status: 'DISCONNECTED' });
-  }
+  handlePeerDisconnect() {}
 
   render() {
     return (
@@ -67,11 +97,11 @@ class App extends Component {
         </div>
         <div>
           <h2>Snapshot</h2>
-          <p></p>
+          <p />
         </div>
         <div>
           <h2>Log</h2>
-          <p></p>
+          <p />
         </div>
       </div>
     );
