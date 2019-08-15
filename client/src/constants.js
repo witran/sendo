e3f2443f7a6e71d7472ea0ebeba18b661e7a04ce
Dung Tran <witran@zendesk.com>
2019-08-15 18:37:26 +0800

    Update message signature

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,17 +1,11 @@
 export const ServerMessages = {
 	Incoming: {
-		// store
 		Data: "data",
-		// coordinator
-		// SetId: "set_id",
 		AddEdge: "add_edge",
 		RemoveEdge: "remove_edge"
 	},
 	Outgoing: {
-		// StartStream: "start_stream",
-		// PeerConnected: "peer_connected",
-		// PeerDisconnected: "peer_disconnected",
-		Ack: "ack",
+		Ack: "data_ack",
 		SetId: "set_id",
 		AddEdgeAck: "add_ege_ack",
 		RemoveEdgeAck: "remove_edge_ack"
